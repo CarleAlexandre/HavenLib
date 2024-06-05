c9dc7fc2eed0cc934abbf154654d5f6e4663c60b
fatmeat <pathofyggdrasil@gmail.com>
2024-06-05 16:55:23 +0200

    working

--- include/haven.hpp
+++ include/haven.hpp
@@ -1,9 +1,9 @@
 #ifndef HAVEN_LIB_HPP
 # define HAVEN_LIB_HPP
 
-#include "haven_def.h"
-#include "haven_file.hpp"
-#include "haven_logic.hpp"
-#include "haven_parser.hpp"
+#include <haven_def.h>
+#include <haven_file.hpp>
+#include <haven_logic.hpp>
+#include <haven_parser.hpp>
 
 #endif
