b576df0fc73f9b6d695517a08363973bbc41e584
fatmeat <pathofyggdrasil@gmail.com>
2024-06-06 17:30:16 +0200

    corrected a non existant bug

--- include/haven_file.hpp
+++ include/haven_file.hpp
@@ -15,7 +15,6 @@ static char *readFile(const char *filepath) {
 		ret = (char *)malloc(str.str().size() + 1);
 		ret[str.str().size()] = 0;
 		str.str().copy(ret, str.str().size());
-		ret[str.str().size()] = '\0';
 		str.clear();
 		//need to free returned value after use;
 		file.close();
